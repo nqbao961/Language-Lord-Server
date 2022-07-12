b8df28948399dff616113397ab7cd316eb4652cf
nqbao961 <nqbao961@gmail.com>
2022-07-12 14:12:04 +0700

    fix build

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['https://language-lord-web.vercel.app', 'http://localhost:3000'],
     credentials: true,
   });
   await app.listen(process.env.PORT || 5000);
